50574715d637f86cedcf03f4932908a77822f19d
Naufal Ramadhan <ramnaufal@gmail.com>
2025-10-30 21:18:43 +0700

    ZOR-2: model transactions update

--- laravel-dashboard/app/Models/Transaction.php
+++ laravel-dashboard/app/Models/Transaction.php
@@ -2,17 +2,37 @@
 
 namespace App\Models;
 
+use App\Models\Base\BaseModel;
 use Illuminate\Database\Eloquent\Model;
 
-class Transaction extends Model
+class Transaction extends BaseModel
 {
     protected $fillable = [
-        'product_id',
-        'customer_id',
-        'quantity',
-        'total_price',
-        'customer_name',
-        'customer_email',
-        'customer_phone'
+        'transactionId',
+        'name',
+        'email',
+        'phone',
+        'station_id',
+        'connector_id',
+        'tariff_code',
+        'executed_price',
+        'midtrans_order_id',
+        'email_status',
+        'wa_status',
+        'payment_status',
+        'manual_stop',
+        'start_time',
+        'stop_time',
+        // timestamps auto-set
     ];
+
+    public function station()
+    {
+        return $this->belongsTo(Station::class, 'station_id');
+    }
+
+    public function connector()
+    {
+        return $this->belongsTo(Connector::class, 'connector_id');
+    }
 }
