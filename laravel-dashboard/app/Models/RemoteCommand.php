43014bdeb9e25feb7e70736e77c766f789f83241
Naufal Ramadhan <ramnaufal@gmail.com>
2025-11-02 00:13:06 +0700

    ZOR-2 : fixing fillable

--- laravel-dashboard/app/Models/RemoteCommand.php
+++ laravel-dashboard/app/Models/RemoteCommand.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class RemoteCommand extends Model
 {
     protected $table = "remote_commands";
+    protected  $fillable = [
+        'station_id',
+        'connector_id',
+        'command',
+        'payload',
+        'status',
+        'created_at',
+        'updated_at'
+    ];
 }
