b2cc97c06c81273f8acd3e0e98d2f3421edb09d1
Naufal Ramadhan <ramnaufal@gmail.com>
2025-11-20 00:29:15 +0700

    ZOR-2: conditionin route

--- laravel-dashboard/routes/web.php
+++ laravel-dashboard/routes/web.php
@@ -8,16 +8,16 @@ use Illuminate\Support\Facades\Route;
 
 // CLIENT
 Route::get('/start/{station_code}/{connector_code}', [HomeController::class, 'start']);
-Route::get('/start/session', [HomeController::class, 'session'])->name('zora.start.session');
-Route::get('/stop', [HomeController::class, 'stop'])->name('zora.stop');
+Route::get('/start/session', [HomeController::class, 'session'])->name('start.session');
+Route::get('/stop', [HomeController::class, 'stop'])->name('stop');
 // Alias to match templates expecting zora.* route names
-Route::post('/stop/action', [HomeController::class, 'stopAction'])->name('zora.stop.action');
+Route::post('/stop/action', [HomeController::class, 'stopAction'])->name('stop.action');
 
-// Route::get('/pay', [PaymentController::class, 'index'])->name('zora.');
-Route::post('/checkout', [PaymentController::class, 'checkout'])->name('zora.checkout');
-Route::get('/checkout/after', [PaymentController::class, 'after'])->name('zora.checkout.after');
-Route::get('/checkout/status/{orderId}', [PaymentController::class, 'status'])->name('zora.checkout.status');
-Route::post('/checkout/notification', [PaymentController::class, 'notification'])->name('zora.checkout.notification'); // endpoint webhook
+// Route::get('/pay', [PaymentController::class, 'index'])->name('');
+Route::post('/checkout', [PaymentController::class, 'checkout'])->name('checkout');
+Route::get('/checkout/after', [PaymentController::class, 'after'])->name('checkout.after');
+Route::get('/checkout/status/{orderId}', [PaymentController::class, 'status'])->name('checkout.status');
+Route::post('/checkout/notification', [PaymentController::class, 'notification'])->name('checkout.notification'); // endpoint webhook
 
 // TEST STOP ROUTE
 // Route::get('/teststop', function () {
