94f603fdf4d178ba8c26ae03cb1d050f7dc8e4c4
Naufal Ramadhan <ramnaufal@gmail.com>
2025-11-20 03:20:45 +0700

    fix

--- laravel-dashboard/resources/views/home/after.blade.php
+++ laravel-dashboard/resources/views/home/after.blade.php
@@ -1,6 +1,3 @@
-@extends('templates.template')
-
-@section('content')
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -15,6 +12,5 @@
         </div>
     </div>
 </div>
-@endsection
 
 
