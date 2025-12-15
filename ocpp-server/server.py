e4eb03b6c7aa37d99b72235c87c7cd2edf085175
ilman30 <ilmannurhakim0@gmail.com>
2025-12-16 01:02:08 +0700

    debug stop

--- ocpp-server/server.py
+++ ocpp-server/server.py
@@ -214,20 +214,23 @@ class ChargePoint(CP16):
                         log.warning('log payload')
                         log.warning(payload)
                         # Tentukan connector_id dulu
-                        connector_id = (
-                            norm.get("connector")
-                            or payload.get("connectorId")
-                            or connector_hint
-                            or 1
-                        )
+                        # connector_id = (
+                        #     norm.get("connector")
+                        #     or payload.get("connectorId")
+                        #     or connector_hint
+                        #     or 1
+                        # )
+                        connector_id = payload.get("connectorId")
 
                         # 1) Coba ambil transactionId dari payload Laravel
                         tx_id = None
-                        tx_raw = (
-                            payload.get("transactionId")
-                            or payload.get("transaction_id")
-                            or payload.get("tx_id")
-                        )
+                        # tx_raw = (
+                        #     payload.get("transactionId")
+                        #     or payload.get("transaction_id")
+                        #     or payload.get("tx_id")
+                        # )
+                        
+                        tx_raw = payload.get("transactionId")
 
                         if tx_raw is not None:
                             try:
