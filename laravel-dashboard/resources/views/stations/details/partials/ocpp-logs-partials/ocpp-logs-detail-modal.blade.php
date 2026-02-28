af28bdd4ee8c0ab44eec0ee6ad3d16ab32af93cb
ilman30 <ilmannurhakim0@gmail.com>
2026-02-28 21:44:07 +0700

    [feature]-[admin] - OCPP Logs Detail

---
+++ laravel-dashboard/resources/views/stations/details/partials/ocpp-logs-partials/ocpp-logs-detail-modal.blade.php
@@ -0,0 +1,32 @@
+<div class="modal fade" id="ocppDetailModal" tabindex="-1" role="dialog" aria-labelledby="stationDetailModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h5 class="modal-title" id="stationDetailModalLabel">OCPP Response</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <label class="font-weight-bold">Payload</label>
+                        <pre id="md_payload" style="background:#e1e1e1;color:#000000;padding:12px;border-radius:6px;max-height:400px;overflow:auto;"></pre>
+                    </div>
+                    <div class="col-md-6">
+                        <label class="font-weight-bold">Response</label>
+                        <pre id="md_response" style="background:#e1e1e1;color:#000000;padding:12px;border-radius:6px;max-height:400px;overflow:auto;"></pre>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
+
+        </div>
+    </div>
+</div>
